91450e1c7239eb0aa2760b8d393f980e7443fa88
kdragon <danny@kdrag0n.dev>
2020-11-24 01:43:42 +0000

    Switch to Skia's Vulkan rendering backend

--- device.mk
+++ device.mk
@@ -102,10 +102,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libdng_sdk.vendor
-
-PRODUCT_PACKAGES += \
-    Snap
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libxml2 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
