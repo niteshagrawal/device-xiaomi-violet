29f0c890bb3a8ef55c1c1cfe5bdd5d3388422a3c
Dark-free <emchaouki@gmail.com>
2021-05-22 15:51:56 +0100

    violet: Add overlay for masking the notch

--- device.mk
+++ device.mk
@@ -279,6 +279,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # NotchBarKiller
 PRODUCT_PACKAGES += \
     NotchBarKiller
