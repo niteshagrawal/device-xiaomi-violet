9934d8332907e3c354baa87f39e6045e11a5c8fb
Andrew Thomas <44932193+itz63c@users.noreply.github.com>
2021-08-20 14:18:53 +0530

    violet: Add Notch Bar Killer overlay

--- device.mk
+++ device.mk
@@ -279,6 +279,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# NotchBarKiller
+PRODUCT_PACKAGES += \
+    NotchBarKiller
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
