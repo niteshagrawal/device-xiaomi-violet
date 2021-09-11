217867e4f5edf73034813f6d28add406cdb41641
niteshagrawal <niteshgupta.ng938@gmail.com>
2021-09-11 15:50:14 +0000

    switch to main.mk

--- stag_violet.mk
+++ stag_violet.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Inherit some common StagOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/stag/config/common.mk)
+$(call inherit-product, vendor/stag/main.mk)
 
 PRODUCT_NAME := stag_violet
 PRODUCT_DEVICE := violet
